6d72bfb41f349dab6e1794e8b6b98edbab4dc408
Jurik-001 <jurik.eineemail@gmail.com>
2024-01-01 22:41:53 +0100

    add openai_client with image support

---
+++ echo_artistry/src/scene_description_generation.py
@@ -0,0 +1,8 @@
+
+
+class SceneDescriptionGenerator:
+    def __init__(self, scene_description):
+        self.scene_description = scene_description
+
+    def generate_description(self):
+        return self.scene_description
